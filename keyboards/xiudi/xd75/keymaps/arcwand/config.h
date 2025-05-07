80a7d229ca272bea47cf06a7a4c69ac6ef202cb4
Robert Gu <arcwandx86@gmail.com>
2025-05-07 13:12:03 -0400

    Remove a layer

--- keyboards/xiudi/xd75/keymaps/arcwand/config.h
+++ keyboards/xiudi/xd75/keymaps/arcwand/config.h
@@ -3,6 +3,11 @@
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
 #define FORCE_NKRO
+
+// Reduce compiled size
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_TAPPING
 #define NO_MUSIC_MODE
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
