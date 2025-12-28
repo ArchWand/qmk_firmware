b679991f37c91f1108515ab568317199d875899d
Robert Gu <arcwandx86@gmail.com>
2025-12-28 03:17:02 -0600

    Version update

--- keyboards/xiudi/xd75/keymaps/arcwand/config.h
+++ keyboards/xiudi/xd75/keymaps/arcwand/config.h
@@ -2,12 +2,13 @@
 
 #undef LOCKING_SUPPORT_ENABLE
 #undef LOCKING_RESYNC_ENABLE
-#define FORCE_NKRO
+#define NKRO_DEFAULT_ON true
 
 // Reduce compiled size
 #define NO_ACTION_ONESHOT
 #define NO_ACTION_TAPPING
-#define NO_MUSIC_MODE
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 0
+
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_SWIRL
